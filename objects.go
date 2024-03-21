0fc5ebadd22a6c28298e9feebcff173d6f94432e
sg3des <sg3des@gmail.com>
2024-03-21 15:17:51 +0300

    add some methods

--- objects.go
+++ objects.go
@@ -1,10 +1,26 @@
 package objects
 
+import "fmt"
+
 type Object struct {
-	ID   string       `json:"id"`
+	ID   string       `json:"id,omitmepty"`
 	Type string       `json:"type"`
-	Name string       `json:"name"`
-	Prob float64      `json:"prob"`
-	BBox [4]float64   `json:"bbox"`
-	Area [][2]float64 `json:"area"`
+	Name string       `json:"name,omitmepty"`
+	Prob float64      `json:"prob,omitmepty"`
+	BBox [4]float64   `json:"bbox,omitmepty"`
+	Area [][2]float64 `json:"area,omitmepty"`
+
+	Data map[string]interface{} `json:"data,omitmepty"`
+}
+
+func (o Object) String() string {
+	if o.Name != "" {
+		return fmt.Sprintf("%s:%s", o.Type, o.Name)
+	}
+
+	return o.Type
+}
+
+func (o Object) IsValid() bool {
+	return o.Type != ""
 }
